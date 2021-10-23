fbbb0f2cb27d5617be399b3b84a6322667d4aba1
Kamil S <kamileksulkowski@gmail.com>
2021-10-23 18:43:46 +0200

    First commit

--- src/Zmienne.kt
+++ src/Zmienne.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
 
     print("Podaj pierwszą liczbę: ")
     var a = readLine()!!.toInt()
-    print("Podaj drugą liczbę: ")
+    print("Podaj drugą liczbę: ") //Test komentarz
     var b = readLine()!!.toInt()
 
     fun dodaj() {
