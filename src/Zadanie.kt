d86ddda658d9220ace2802d3a910a6ba3330ca51
Marcin Kacik <marcinkacik@yahoo.com>
2021-10-23 19:25:42 +0200

    Zadanie do wykonania

---
+++ src/Zadanie.kt
@@ -0,0 +1,24 @@
+/*
+Zadanie
+
+Program zgodnie ze specyfikacją:
+
+1 - proszenie usera o dane
+2 - menu wyboru (1 do 3)
+3 - liczenie pola kwadrata
+4 - liczenie pola trójkąta
+5 - liczenie pola prostokąta
+
+Funkcja wyświetlająca komunikaty wyswietl()
+Funkcja pobierzDane()
+
+Wraz z pisaniem dodajemy kolejne funkcje
+Kolejne kroki: logowanie/rejestracja, interfejs graficzny.
+
+Kod programu poniżej tego komentarza
+ */
+
+fun main(){
+
+
+}
