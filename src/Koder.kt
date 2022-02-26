b17e17d8f446335f65318b5c091c40ca98804d30
Kamil S <kamileksulkowski@gmail.com>
2022-02-26 18:42:09 +0100

    koder v2

--- src/Koder.kt
+++ src/Koder.kt
@@ -4,6 +4,10 @@ var konieckoder: Boolean = false
 var menuKodera: Int = 0
 var tekstdekoder: String = ""
 var dekoder: Char = '\u0000'
+var dekoderPetla = 1
+var iloscznakow = 0
+var iloscpetli = 0
+var licznik = 0
 
 fun main (args: Array<String>){
     println("Witaj!")
@@ -14,6 +18,8 @@ fun main (args: Array<String>){
 }
 
 fun wyswietlMenuKodera (){
+    println("")
+    println("")
     println("1. Kodowanie")
     println("2. Dekodowanie")
     println("3. Koniec")
@@ -43,21 +49,31 @@ return
 fun wprowadzTekst (){
     println("Wprowadź tekst do zakodowania.")
     tekst = readLine()!!
+    println("")
 }
 
 fun kodowanie (){
     tekst.forEach {
-        println(it.toChar().hashCode())
+        print(it.toChar().hashCode())
     }
 }
 
 fun wprowadzTekstDekoder (){
     println("Wprowadź tekst do odkodowania.")
     tekstdekoder = readLine()!!
+    println("")
 }
 
-fun dekodowanie (){
+fun dekodowanie () {
     tekstdekoder
-    dekoder = tekstdekoder.toInt().toChar()
-    println(dekoder)
+
+    iloscznakow = tekstdekoder.length
+    iloscpetli = iloscznakow / 2
+
+    for(x in 0..iloscpetli)
+    print((tekstdekoder[licznik+2] + "" + tekstdekoder[licznik+2]).toInt().toChar())
+    println("")
 }
+
+
+
